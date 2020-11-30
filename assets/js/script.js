b0a0738c0ed4a001c76474574c7f10748b898709
gint.stirbys <gint.stirbys@gmail.com>
2020-11-29 19:52:47 -0500

    comments in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,7 @@ function loadEventListeners() {
     
 }
 
-//get cities from Local Storage and display- I adapted the method shown by 
+//get cities from Local Storage and display- I adapted the method shown by Brad Traversy in his Udemy JavaScript course
 function getCities() {
     let cities;
     if(localStorage.getItem('cities') === null) {
