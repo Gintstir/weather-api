2b07dfbc17651065fb664c46d863d50c0c460dde
gint.stirbys <gint.stirbys@gmail.com>
2020-12-03 13:20:01 -0500

    recently searched cities are now clickable, if search field is null, list will not populate and will return alert instead

---
+++ Weather Dashboard.md
@@ -0,0 +1,69 @@
+# Weather Dashboard
+
+This is the completed sixth weekly challenge for the UPENN LPS Coding Bootcamp.<br><br>
+This project required us to build a weather dashboard using [openweathermap.org](https://openweathermap.org/).<br>
+According to the grading rubric the scheduler had to meet the following requirements:<br>
+
+```
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+WHEN I view the UV index
+THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+```
+
+<br>
+
+## **Link**
+
+Here is a link to the [deployed application](https://gintstir.github.io/weather-api/)<br> 
+
+Here is a link to the [github repository](https://github.com/Gintstir/weather-api)<br>
+
+
+## **Screenshots**  
+The mock up we were to use as a guide:
+
+>![Mock-up](./assets/images/06-server-side-apis-homework-demo-resized.png)
+
+
+The deployed site:
+
+>![Deployed-Weather-Dashboard](./assets/images/deployed-dashboard.png)
+<br>
+
+## **Motivation**
+
+
+- Learning to retrieve and display data from a third-part API.
+- Gaining a better understanding of vanilla JS syntax and structure.
+- Using localStorage
+- Learning about and implementing:
+    - DOM manipulation
+    - Using Third Party APIs to style and improve UI<br>  
+
+## **Build Status**
+
+This Work Day Scheduler should be considered complete.    
+
+## **Code Style**
+
+This project was built from the ground up using:
+- CSS
+- HTML
+- JavaScript
+- jQuery
+- <a href="https://getbootstrap.com/" target="_blank">Bootstrap</a>
+- <a href="https://momentjs.com/" target="_blank">Moment.js</a>
+- <a href="https://fontawesome.com/" target="_blank">FontAwesome</a>
+- <a href="https://openweathermap.org/" target="_blank">Openweathermap API</a>
+
+## **Credits**
+
+Thanks to UPennLPS Coding Bootcamp. Thank you to the bootcamp instructors and TA's for all their help along the way ❤.  
--- assets/js/script.js
+++ assets/js/script.js
@@ -16,10 +16,10 @@ function loadEventListeners() {
 
     //Load saved cities section
     document.addEventListener('DOMContentLoaded', getCities);
-    
+
     //get current weather and 5 day forecast
     document.querySelector("#search-button").addEventListener("click", getCurrentWeather);
-    
+
 }
 
 //get cities from Local Storage and display- I adapted the method shown by Brad Traversy in his Udemy JavaScript course.
@@ -27,45 +27,54 @@ function getCities() {
 
     let cities;
 
-    if(localStorage.getItem('cities') === null) {
+    if (localStorage.getItem('cities') === null) {
         cities = [];
     } else {
         cities = JSON.parse(localStorage.getItem('cities'));
     }
 
-    cities.forEach(function(city){
+    cities.forEach(function (city) {
         // Create li element
         const cityList = document.createElement('li');
         // Add class
         cityList.className = 'cityContainer-item list-group-item';
         // Create text node and append to li
         cityList.appendChild(document.createTextNode(city));
+
+        cityList.addEventListener('click', function () {
+            cityWeather(this.textContent);
+        })
         // Append li to ul
         searchedCityList.appendChild(cityList);
-    });    
+    });
 }
 
 //get value from search bar and make list of searched cities
 function getCurrentWeather(event) {
-    event.preventDefault();    
+    event.preventDefault();
     //console.log("this is working");
 
-    var cityNameEl = document.querySelector("#cityName").value; 
-    
+    var cityNameEl = document.querySelector("#cityName").value;
+
     if (cityNameEl === "") {
         alert("Please add a city name to see that city's weather");
-        //break;
+        return;
     }
+
     const cityList = document.createElement('li');
     // Add class
     cityList.className = 'cityContainer-item list-group-item';
     // Create text node and append to li
     cityList.appendChild(document.createTextNode(cityNameEl));
+    //add on click
+    cityList.addEventListener('click', function () {
+        cityWeather(this.textContent);
+    })
     // Append li to ul
     searchedCityList.appendChild(cityList);
+    
 
-
-    cityWeather(cityNameEl);    
+    cityWeather(cityNameEl);
 
     //store in LocalStorage
     storeCityInLocalStorage(cityNameEl);
@@ -75,37 +84,46 @@ function getCurrentWeather(event) {
 //store city
 function storeCityInLocalStorage(city) {
     let cities;
-    if(localStorage.getItem('cities') === null) {
+    if (localStorage.getItem('cities') === null) {
         cities = [];
     } else {
         cities = JSON.parse(localStorage.getItem('cities'));
     }
 
     cities.push(city);
-    
+
     localStorage.setItem('cities', JSON.stringify(cities));
 }
 
 //fetch call for current weather
 function cityWeather(city) {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        //console.log(data);        
-        displayTodayWeather(data);
-        fiveDayForecast(city);
+        .then(function (response) {
+            return response.json()
+
+        })
+
+
+        .then(function (data) {
+            // //console.log(data);        
+            displayTodayWeather(data);
+            fiveDayForecast(city);
+
+        })
+
+        .catch(err => {
+            
         
-    })
-    .catch(err => console.log(err));
+            console.log(err);
+        })
+
 
 }
 
 
 //display current weather
 function displayTodayWeather(data) {
-    
+
     //weather icon    
     var currentIcon = document.querySelector("#dailyIcon");
     currentIcon.innerHTML = "";
@@ -114,7 +132,7 @@ function displayTodayWeather(data) {
     var currentWeatherImage = document.createElement("img");
     //larger img for current weather section
     currentWeatherImage.setAttribute("src", "http://openweathermap.org/img/wn/" + currentWeatherIcon + "@2x.png");
-    currentWeatherImage.className = "my-auto";   
+    currentWeatherImage.className = "my-auto";
     currentIcon.appendChild(currentWeatherImage);
 
     //City name and date
@@ -124,11 +142,11 @@ function displayTodayWeather(data) {
     //use moment.js here for current date
     currentCityDate.textContent = (data.name + " " + "(" + currentTime + ")");
     currentWeatherCityDate.appendChild(currentCityDate);
-    
+
     //container for temp, humidity, wind, and uv index
     var currentWeather = document.querySelector("#daily-conditions");
     currentWeather.innerHTML = "";
-    
+
     //create elements and text for temp/humidity/and wind- make seperate function for UV index
     var currentTempLi = document.createElement("p");
     currentTempLi.textContent = ("Temp: " + data.main.temp + "°F");
@@ -137,11 +155,13 @@ function displayTodayWeather(data) {
     var currentWindLi = document.createElement("p");
     currentWindLi.textContent = ("Wind Speed: " + data.wind.speed + "mph");
     currentUvIndex(data);
-    
+
     //append new text and data to currentweather element
     currentWeather.appendChild(currentTempLi);
     currentWeather.appendChild(currentHumidityLi);
     currentWeather.appendChild(currentWindLi);
+
+    // create li button for city
 
 };
 
@@ -150,50 +170,50 @@ function displayTodayWeather(data) {
 //fetch and display the five day forecast
 function fiveDayForecast(cityNameEl) {
 
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityNameEl + "&appid=" + apiKey + "&units=imperial" )
-    .then(function(response) {
-        return response.json();
-        
-    })
-    .then(function(data) {
-        // to cycle through 5 day id's
-        let dayPosition = 0;
-        //console.log(data);
-        for (let i = 0; i < data.list.length; i++)  {
-            //TA Symone Varnado went over this with me( this loops through the api data array
-            // for the five day forecast which iterates at 3 hrs intervals for 5 days and only catches every day at 3pm(15:00:00) )
-            if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
-                //get date for each day- Use method of using split from SO to parse out date from object array
-                var day = data.list[i].dt_txt.split("-")[2].split(" ")[0];
-                var month = data.list[i].dt_txt.split("-")[1];
-                var year = data.list[i].dt_txt.split("-")[0];
-                //console.log(day, month, year);
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityNameEl + "&appid=" + apiKey + "&units=imperial")
+        .then(function (response) {
+            return response.json();
 
-                //display date in forecast cards
-                var fiveDayDateHeader = document.querySelector("#" + "forecastDate" + dayPosition);
-                fiveDayDateHeader.textContent = ("(" + month + "-" + day + "-" + year + ")");
-                //console.log(fiveDayDateHeader);
+        })
+        .then(function (data) {
+            // to cycle through 5 day id's
+            let dayPosition = 0;
+            //console.log(data);
+            for (let i = 0; i < data.list.length; i++) {
+                //TA Symone Varnado went over this with me( this loops through the api data array
+                // for the five day forecast which iterates at 3 hrs intervals for 5 days and only catches every day at 3pm(15:00:00) )
+                if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+                    //get date for each day- Use method of using split from SO to parse out date from object array
+                    var day = data.list[i].dt_txt.split("-")[2].split(" ")[0];
+                    var month = data.list[i].dt_txt.split("-")[1];
+                    var year = data.list[i].dt_txt.split("-")[0];
+                    //console.log(day, month, year);
 
-                //display temp and humidity in forecast cards                
-                var fiveDayTempP = document.querySelector("#" + "forecastTemp" + dayPosition);
-                fiveDayTempP.textContent = ("Temp: " + data.list[i].main.temp);
-                var fiveDayHumP = document.querySelector("#" + "forecastHum" + dayPosition);
-                fiveDayHumP.textContent = ("Humidity: " + data.list[i].main.humidity + "%")
-                //console.log(fiveDayTempP);
+                    //display date in forecast cards
+                    var fiveDayDateHeader = document.querySelector("#" + "forecastDate" + dayPosition);
+                    fiveDayDateHeader.textContent = ("(" + month + "-" + day + "-" + year + ")");
+                    //console.log(fiveDayDateHeader);
 
-                //display 5 day image
-                var fiveDayImg = document.querySelector("#" + "forecastImg" + dayPosition);
-                fiveDayImg.setAttribute("src", "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png");
+                    //display temp and humidity in forecast cards                
+                    var fiveDayTempP = document.querySelector("#" + "forecastTemp" + dayPosition);
+                    fiveDayTempP.textContent = ("Temp: " + data.list[i].main.temp);
+                    var fiveDayHumP = document.querySelector("#" + "forecastHum" + dayPosition);
+                    fiveDayHumP.textContent = ("Humidity: " + data.list[i].main.humidity + "%")
+                    //console.log(fiveDayTempP);
+
+                    //display 5 day image
+                    var fiveDayImg = document.querySelector("#" + "forecastImg" + dayPosition);
+                    fiveDayImg.setAttribute("src", "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png");
 
 
 
-                //console.log(data.list[i].weather[0].icon);
+                    //console.log(data.list[i].weather[0].icon);
 
-                //move through img, temp, and humidity positions
-                dayPosition++;
+                    //move through img, temp, and humidity positions
+                    dayPosition++;
+                }
             }
-        }        
-    })
+        })
 };
 
 //fetch and display uv index on current weather
@@ -201,35 +221,35 @@ function currentUvIndex(data) {
     //console.log(data)
     //fetch call for UV index
     fetch("https://api.openweathermap.org/data/2.5/uvi?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&appid=" + apiKey)
-    .then(function(response) {
-        return response.json();
-        
-    })
-    .then(function(data) {
-        //  create p tag to hold info
-        var uvIndexHTML = document.createElement('p');
-        uvIndexHTML.textContent =  'UV Index: '
-        // create a div to append to our p tag
-        var uvNum = document.createElement('div')
-        uvNum.textContent = data.value;
-        uvIndexHTML.append(uvNum)
+        .then(function (response) {
+            return response.json();
 
-        //change uvNUm with bootstrap class depending on UVIndex value
-        
-        if(data.value <= 4.0 ) {           
-            uvNum.setAttribute('class', "btn btn-success")
-        }
-        
-        if(data.value > 4 && data.value <= 8) {
-            uvNum.setAttribute('class', "btn btn-warning")
-        }
-        if(data.value > 8) {
-            uvNum.setAttribute('class', 'btn btn-danger');
-        }
-        
+        })
+        .then(function (data) {
+            //  create p tag to hold info
+            var uvIndexHTML = document.createElement('p');
+            uvIndexHTML.textContent = 'UV Index: '
+            // create a div to append to our p tag
+            var uvNum = document.createElement('div')
+            uvNum.textContent = data.value;
+            uvIndexHTML.append(uvNum)
 
-        var currentWeather = document.querySelector("#daily-conditions").append(uvIndexHTML) 
-    })
+            //change uvNUm with bootstrap class depending on UVIndex value
+
+            if (data.value <= 4.0) {
+                uvNum.setAttribute('class', "btn btn-success")
+            }
+
+            if (data.value > 4 && data.value <= 8) {
+                uvNum.setAttribute('class', "btn btn-warning")
+            }
+            if (data.value > 8) {
+                uvNum.setAttribute('class', 'btn btn-danger');
+            }
+
+
+            var currentWeather = document.querySelector("#daily-conditions").append(uvIndexHTML)
+        })
     //console.log(currentUvIndex(cityLat, cityLong));
 };
 
